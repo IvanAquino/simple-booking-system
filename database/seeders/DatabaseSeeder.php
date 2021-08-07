808b2eae2cdfffc6573ea13ccf12eb0b08328e21
Ivan Aquino <ivnaqn@gmail.com>
2021-08-07 13:03:19 -0500

    Added models employee, service and schedule

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Employee;
+use App\Models\Service;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $employee_1 = Employee::create([
+            'name' => 'Employee 1',
+        ]);
+        $employee_2 = Employee::create([
+            'name' => 'Employee 2',
+        ]);
+
+        $service_1 = Service::create([
+            'name' => 'Coding session',
+            'duration' => 60,
+        ]);
+        $service_2 = Service::create([
+            'name' => 'Code review',
+            'duration' => 120,
+        ]);
+
+        $employee_1->services()->attach([
+            $service_1->id,
+            $service_2->id,
+        ]);
+        $employee_2->services()->attach([
+            $service_1->id,
+            $service_2->id,
+        ]);
     }
 }
