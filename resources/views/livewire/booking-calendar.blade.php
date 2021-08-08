b62990eb85e9605680699b3a4e2dce6a1596da7e
Ivan Aquino <ivnaqn@gmail.com>
2021-08-08 12:29:07 -0500

    Separate calendar component and added logic to navigate between weeks

---
+++ resources/views/livewire/booking-calendar.blade.php
@@ -0,0 +1,43 @@
+<div class="bg-white rounded-lg">
+    <div class="flex items-center justify-center relative">
+        @if($this->weekIsGreaterThanCurrent)
+            <button type="button" class="p-4 absolute left-0 top-0" wire:click="decrementCalendarWeek">
+                Prev
+            </button>
+        @endif
+
+        <div class="text-lg font-semibold p-4">
+            {{ $calendarStartDate->format('M Y') }}
+        </div>
+
+        <button type="button" class="p-4 absolute right-0 top-0" wire:click="incrementCalendarWeek">
+            Next
+        </button>
+    </div>
+
+    <div class="flex justify-between items-center px-3 border-b border-gray-200 pb-2">
+        @foreach($this->calendarWeekInterval as $day)
+            <button type="button" class="text-center group cursor-pointer focus:outline-none">
+                <div class="text-xs leading-none mb-1 text-gray-700">
+                    {{ $day->format('D') }}
+                </div>
+                <div class="text-lg leading-none p-1 rounded-full w-9 h-9 group-hover:bg-gray-200 flex items-center justify-center">
+                    {{ $day->format('d') }}
+                </div>
+            </button>
+        @endforeach
+    </div>
+
+    <div class="max-h-52 overflow-y-scroll">
+        <div class="flex flex-row items-center border-b border-gray-200">
+            <input type="radio" name="time" id="" value="" class="" />
+            <label for="" class="text-left focus:outline-none px-4 py-2 cursor-pointer flex items-center">
+                09:00
+            </label>
+        </div>
+    </div>
+
+    <div class="text-center text-gray-700 px-4 py-2">
+        No available slots
+    </div>
+</div>
--- resources/views/livewire/create-booking.blade.php
+++ resources/views/livewire/create-booking.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-gray-100 max-w-sm mx-auto m-6 p-5 rounded-lg">
+<div class="bg-gray-200 max-w-sm mx-auto m-6 p-5 rounded-lg">
     <form>
 
         <div class="mb-6">
@@ -32,44 +32,7 @@
             </label>
         </div>
 
-        <div class="bg-white rounded-lg">
-            <div class="flex items-center justify-center relative">
-                <button type="button" class="p-4 absolute left-0 top-0">
-                    Prev
-                </button>
-
-                <div class="text-lg font-semibold p-4">
-                    Mes 2021
-                </div>
-
-                <button type="button" class="p-4 absolute right-0 top-0">
-                    Next
-                </button>
-            </div>
-
-            <div class="flex justify-between items-center px-3 border-b border-gray-200 pb-2">
-                <button type="button" class="text-center group cursor-pointer focus:outline-none">
-                    <div class="text-xs leading-none mb-1 text-gray-700">Mon</div>
-                    <div class="text-lg leading-none p-1 rounded-full w-9 h-9 group-hover:bg-gray-200 flex items-center justify-center">
-                        1
-                    </div>
-                </button>
-            </div>
-
-            <div class="max-h-52 overflow-y-scroll">
-                <div class="flex flex-row items-center border-b border-gray-200">
-                    <input type="radio" name="time" id="" value="" class="" />
-                    <label for="" class="text-left focus:outline-none px-4 py-2 cursor-pointer flex items-center">
-                        09:00
-                    </label>
-                </div>
-            </div>
-
-            <div class="text-center text-gray-700 px-4 py-2">
-                No available slots
-            </div>
-        </div>
-
+        <livewire:booking-calendar />
 
     </form>
 </div>
